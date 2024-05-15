a855dc0c9c068a433b69711a3b79eac72e9b437b
mano3578 <mano3578@CSL0693.global.chainsys.com>
2024-05-15 18:16:31 +0530

    worked in servlet

---
+++ DemoApp1/src/main/java/com/chainsys/myservlet/SecondServlet.java
@@ -0,0 +1,47 @@
+package com.chainsys.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/activeServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String firstName=request.getParameter("fname");
+		int phoneNo=Integer.parseInt(request.getParameter("lname"));
+		PrintWriter out=response.getWriter();
+		System.out.println(firstName);
+		out.println(firstName);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
