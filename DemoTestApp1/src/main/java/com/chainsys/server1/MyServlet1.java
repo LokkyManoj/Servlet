ce23278a35966c47528fba792710f885e382fd0d
mano3578 <mano3578@CSL0693.global.chainsys.com>
2024-05-16 11:08:21 +0530

    Added Response.ReDirect in servlet and added bg image

---
+++ DemoTestApp1/src/main/java/com/chainsys/server1/MyServlet1.java
@@ -0,0 +1,57 @@
+package com.chainsys.server1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet1
+ */
+@WebServlet("/activeServlet")
+public class MyServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("username");
+		String Email=request.getParameter("email");
+		
+		String phone=request.getParameter("mobile");
+        PrintWriter out = response.getWriter();
+
+        System.out.println(firstName);
+        System.out.println(Email);
+        System.out.println(phone);
+        
+   out.println(firstName);
+   out.println(Email);
+   out.println(phone);
+
+   response.sendRedirect("UserCred.html");
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
